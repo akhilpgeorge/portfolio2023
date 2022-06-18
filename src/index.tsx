be6412cec20930847fcacfdce87ebdb03efc4a87
akhilpgeorge <akhilpgeorge@gmail.com>
2022-06-18 23:39:21 +0530

    formats

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const container:HTMLElement | null = document.getElementById('app');
+const container:HTMLElement | null = document.getElementById("app");
 const root = ReactDOM.createRoot(container as HTMLElement);
 root.render(
   <div>Hello</div>
-)
+);
