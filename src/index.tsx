119310302ebd158b0c8547b9bd540780f068641b
akhilpgeorge <akhilpgeorge@gmail.com>
2022-12-12 21:35:44 +0530

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/styles.scss";
 
 const container: HTMLElement | null = document.getElementById("app");
 const root = ReactDOM.createRoot(container as HTMLElement);
-root.render(<div>Hello</div>);
+root.render(<h1 className="text-3xl">
+    Hello world!
+</h1>);
